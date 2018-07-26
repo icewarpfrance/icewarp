03bd5eb314cdf3b9fdd963667484f7deddb01842
icewarpfrance <41593027+icewarpfrance@users.noreply.github.com>
2018-07-26 12:56:42 +0200

    Create icewarpForever.sh

---
+++ 12.1.1.2_DEB8_x64/icewarpForever.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -e
+stopIceWarpService()
+{
+   /usr/sbin/service icewarp stop
+   exitStatus=$?
+   /bin/kill -TERM "${childPid}" 2>/dev/null
+   exit $exitStatus
+}
+if test -f /opt/icewarp/calendar.tgz
+then
+   /bin/chown icewarp:icewarp /opt/icewarp/config
+   /bin/chown icewarp:icewarp /opt/icewarp/mail
+   /bin/chown icewarp:icewarp /opt/icewarp/logs
+   /bin/chown icewarp:icewarp /opt/icewarp/temp
+   /bin/chown icewarp:icewarp /opt/icewarp/spam
+   /bin/chown icewarp:icewarp /opt/icewarp/calendar
+   cd /opt/icewarp
+   tar xzf calendar.tgz calendar/
+   tar xzf spam.tgz     spam/
+   tar xzf config.tgz   config/
+   rm calendar.tgz spam.tgz config.tgz
+   if test -z "$PUBLICIP"
+   then
+      PUBLICIP=`wget http://ipecho.net/plain -O - -q`
+   fi
+   if test -z "$LOCALIP"
+   then
+      LOCALIP=`ip -o -4 a s eth0 | awk '{sub(/\/.*/, "", $4);print $4}'`
+   fi
+   if test -z "$DNSSERVER"
+   then
+      DNSSERVER=`grep -i '^nameserver' /etc/resolv.conf |head -n1|cut -d ' ' -f2`
+   fi
+   /opt/icewarp/tool.sh set system  c_system_services_sip_localaccesshost  $LOCALIP
+   /opt/icewarp/tool.sh set system  c_system_services_sip_remoteaccesshost $PUBLICIP
+   /opt/icewarp/tool.sh set system  c_mail_smtp_general_dnsserver          $DNSSERVER
+fi
+/usr/sbin/service icewarp start
+trap stopIceWarpService TERM
+sleep infinity &
+childPid=$!
+wait ${childPid}
+trap - TERM
+wait ${childPid}
