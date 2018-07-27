98392b68bf3b8330340bc5940654db0cb5e7d25e
icewarpfrance <41593027+icewarpfrance@users.noreply.github.com>
2018-07-27 18:30:28 +0200

    Update license.sh

--- 12.1.1.2_DEB8_x64/license.sh
+++ 12.1.1.2_DEB8_x64/license.sh
@@ -3,11 +3,11 @@ trapExit() {
    exit 1
 }
 trap trapExit INT
-more "/opt/icewarp/licences/LICENSE"
+/usr/bin/iconv -f ISO-8859-1 -t utf8 LICENSE "/opt/icewarp/licence/LICENSE" | /usr/bin/more
 echo ""
 good "You must accept this license agreement if you want to continue."
 good "Press ENTER to accept license or CTRL+C to quit"
 echo ""
 read -s
-mv /opt/icewarp/licenses/accept.sh /opt/icewarp/licenses/accepted.sh
+mv /opt/icewarp/license/accept.sh /opt/icewarp/license/accepted.sh
 trap INT
