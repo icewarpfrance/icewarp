83f03157bdbb9e29b93fb69165f2e8ccfb89d597
icewarpfrance <41593027+icewarpfrance@users.noreply.github.com>
2018-07-29 22:03:02 +0200

    Create license.sh

---
+++ centos7/license.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source /opt/icewarp/scripts/inc/functions.sh
+trapExit() {
+   stty sane
+   exit 1
+}
+trap trapExit INT
+/usr/bin/iconv -f ISO-8859-1 -t utf8 /opt/icewarp/license/LICENSE | /bin/more
+echo ""
+good "You must accept this license agreement if you want to continue."
+good "Press ENTER to accept license or CTRL+C to quit"
+echo ""
+read -s
+trap INT
+mv /opt/icewarp/license/license.sh /opt/icewarp/license/licenseAccepted.sh
+exit 0
